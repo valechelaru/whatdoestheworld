e43a70832bb68f69dd6d0bec53eb21585745b995
Valentin Chelaru <valechelaru@gmail.com>
2020-04-25 15:26:38 +0200

    Extraction of News Titles, Descriptions and Categories

--- main.py
+++ main.py
@@ -34,8 +34,38 @@ def saveToFile(filename, data):
     f.write(data)
     f.close()
 
+def handleXML(dom):
+    handleXMLTitle(dom.getElementsByTagName('title')[0])
+    pubDate = dom.getElementsByTagName('pubDate')[0]
+    print(pubDate.firstChild.data)
+
+def handleXMLTitle(titel):
+    print(getText(titel.childNodes))
+
+def getText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
 if __name__ == '__main__':
     # XML Location of the NYTimes Home Page
-    url = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
-    dom = getXML(url)
+    nytimes_url = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+    dom = getXML(nytimes_url)
     saveToFile('test.xml', dom.toprettyxml())
+
+    handleXML(dom)
+
+    items = dom.getElementsByTagName('item')
+    for item in items:
+        title = item.getElementsByTagName('title')[0]
+        print(title.firstChild.data)
+        description = item.getElementsByTagName('description')[0]
+        try:
+            print(description.firstChild.data)
+        except:
+            print('NO DESCRIPTION!')
+        categorys = item.getElementsByTagName('category')
+        for category in categorys:
+            print(category.firstChild.data)
